cde73b97d3980e85af4396b9fa6afbacd83b4a86
Jonas <jonas.cheng+trello@gmail.com>
2022-01-09 17:51:58 +0800

    refactor: script refinement

--- incronrsync.sh
+++ incronrsync.sh
@@ -10,11 +10,16 @@ esac
 SYNC_PATH=/home/vagrant/shared
 DST_PATH=/home/vagrant
 
-[ "$2" == "$(basename -- $0)" ] && exit
-[ "$2" == "log" ] && exit
-[ "$2" == "" ] && FILE="NULL" || FILE="$2"
+# ignore this script
+[[ ${2} == "$(basename -- ${0})" ]] && exit
+# ignore file *.log
+[[ ${2} == *.log ]] && exit
+# ignore file .*
+[[ ${2} == .* ]] && exit
+# ignore empty and null
+[ ${2} == "" ] && FILE="NULL" || FILE=${2}
 
-echo "$(date "+%H:%M:%S.%3N") OnInotifyReceived $1, $2, $3" >> '/var/log/rsync.log' 2>&1
+echo "$(date "+%H:%M:%S.%3N") OnInotifyReceived ${1}, ${2}, ${3}" >> '/var/log/rsync.log' 2>&1
 
 for dst_server in 10.1.0.10 10.1.0.20 10.1.0.30
 do
@@ -27,8 +32,9 @@ do
     # -t, --times preserve modification times
     # -v, --verbose increase verbosity
     # -z, --compress compress file data during the transfer
-    echo "$(date "+%H:%M:%S.%3N") CMD /usr/bin/rsync -vzrtopg –delete ${SYNC_PATH} ${dst_server}:${DST_PATH}" >> '/var/log/rsync.log' 2>&1
-    /usr/bin/rsync -vzrtopg --delete --progress ${SYNC_PATH} ${dst_server}:${DST_PATH}
+    echo "$(date "+%H:%M:%S.%3N") CMD /usr/bin/rsync -vzrtopg --delete --progress ${1}/${2} ${dst_server}:${1}" >> '/var/log/rsync.log' 2>&1
+    # /usr/bin/rsync -vzrtopg --delete --progress --exclude '.*' ${SYNC_PATH} ${dst_server}:${DST_PATH}
+    /usr/bin/rsync -vzrtopg --delete --progress ${1}/${2} ${dst_server}:${1}
     echo "$(date "+%H:%M:%S.%3N") SYNCED ${dst_server}, ${FILE}" >> '/var/log/rsync.log' 2>&1
   fi
 done
